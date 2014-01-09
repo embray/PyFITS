49d5caab23a2df416052fc2eb9053b1535281080
Erik M. Bray <embray@stsci.edu>
2014-01-09 18:02:33 -0500

    Progress on schemas that actually do something: First a modicum of validation for schema definitions, though there's more to be done on that front.  Next, implementation of the first schema property: mandatory.

--- lib/pyfits/schema.py
+++ lib/pyfits/schema.py
@@ -1,4 +1,81 @@
+import warnings
+
+from pyfits.card import KEYWORD_LENGTH
+
+
+__all__ = ['Schema']
+
+
+class SchemaError(Exception):
+    """Base class for schema-related exceptions."""
+
+    def __init__(self, name, message):
+        self.name = name
+        self.message = message
+        super(SchemaError, self).__init__(name, message)
+
+    def __str__(self):
+        return '%s in %s: %s' % (self.__class__.__name__, self.name,
+                                 self.message)
+
+class SchemaDefinitionError(SchemaError):
+    """Exception raised when a FITS schema definition is not valid."""
+
+
+class SchemaValidationError(SchemaError):
+    """Exception raised when a Schema does not validate a FITS header."""
+
+# TODO: Currently we raise an exception as soon as a violation of a schema
+# is detected; in the future we will collect all schema violations and
+# encapsulate them in a single SchemaValidationError
+
+
+class MetaSchema(type):
+    keyword_properties = set(['mandatory'])
+
+    def __new__(mcls, name, bases, members):
+        keywords = members.setdefault('keywords', {})
+        # For standard FITS keywords, ensure that all keyword definitions are
+        # normalized to uppercase
+        for keyword in list(keywords):
+            if len(keyword) <= KEYWORD_LENGTH and keyword != keyword.upper():
+                warnings.warn(
+                    'Keyword %s should be listed in all caps in schema %s' %
+                    (keyword, name))
+                keywords[keyword.upper()] = keywords[keyword]
+                del keywords[keyword]
+
+        for keyword, properties in keywords.items():
+            # validate each of the keyword properties
+            for propname, value in properties.items():
+                if propname not in mcls.keyword_properties:
+                    raise SchemaDefinitionError(name,
+                        'invalid keyword property for %r: %r; valid keyword '
+                        'properties are: %r' %
+                        (keyword, propname, sorted(mcls.keyword_properties)))
+
+        return super(mcls, mcls).__new__(mcls, name, bases, members)
+
+    def __contains__(cls, keyword):
+        # Seems trivial for now, but will become more complicated to support
+        # indexed keywords like NAXISn
+        return keyword.upper() in cls.keywords
+
+
 class Schema(object):
+    __metaclass__ = MetaSchema
+
     @classmethod
     def validate(cls, header):
+        for keyword, properties in cls.keywords.items():
+            for propname, propval in properties.items():
+                validator = getattr(cls, '_validate_%s' % propname)
+                validator(header, keyword, propval)
+
         return True
+
+    @classmethod
+    def _validate_mandatory(cls, header, keyword, mandatory):
+        if mandatory and keyword not in header:
+            raise SchemaValidationError(cls.__name__,
+                'mandatory keyword %r missing from header' % keyword)
--- lib/pyfits/tests/test_schema.py
+++ lib/pyfits/tests/test_schema.py
@@ -1,7 +1,11 @@
 from __future__ import division, with_statement
 
 import pyfits as fits
+from pyfits.schema import SchemaDefinitionError, SchemaValidationError
 from pyfits.tests import PyfitsTestCase
+from pyfits.tests.util import catch_warnings
+
+from nose.tools import assert_raises
 
 
 class TestSchema(PyfitsTestCase):
@@ -15,5 +19,95 @@ class TestSchema(PyfitsTestCase):
 
         assert EmptySchema.validate(fits.Header())
 
-        h = Header([('TEST', 'TEST')])
+        h = fits.Header([('TEST', 'TEST')])
         assert EmptySchema.validate(h)
+
+    def test_trivial_schema(self):
+        """
+        A schema that lists keywords but does not place *any* requirements
+        on those keywords is as good as an empty schema.
+        """
+
+        h1 = fits.Header([('TEST2', 'TEST')])
+        h2 = fits.Header([('TEST2', 'TEST')])
+
+        class TrivialSchema_1(fits.Schema):
+            keywords = {
+                'TEST1': {}
+            }
+
+        class TrivialSchema_2(fits.Schema):
+            keywords = {
+                'TEST1': {},
+                'TEST2': {},
+                'TEST3': {}
+            }
+
+        assert TrivialSchema_1.validate(fits.Header())
+        assert TrivialSchema_1.validate(h1)
+        assert TrivialSchema_1.validate(h2)
+        assert TrivialSchema_2.validate(fits.Header())
+        assert TrivialSchema_2.validate(h1)
+        assert TrivialSchema_2.validate(h2)
+
+    def test_schema_contains_keyword(self):
+        """
+        Test whether a schema defines a particular keyword (even if trivially).
+        """
+
+        with catch_warnings(record=True) as w:
+            class TrivialSchema(fits.Schema):
+                keywords = {
+                    'TEST1': {},
+                    'TesT2': {},
+                    'TEST3': {}
+                }
+
+        assert len(w) == 1
+
+        assert 'TEST1' in TrivialSchema
+        assert 'TEST2' in TrivialSchema
+        assert 'TEST3' in TrivialSchema
+        assert 'TEST4' not in TrivialSchema
+
+        # Keywords not listed in upper-case should have been normalized
+        assert 'TesT2' not in TrivialSchema.keywords
+        assert 'TEST2' in TrivialSchema.keywords
+
+        # Keywords in schemas are also case-insensitive (at least for standard
+        # keywords; this might not hold for HIERARCH or RVKC keywords if they
+        # are supported)
+        assert 'tEsT1' in TrivialSchema
+
+    def test_invalid_keyword_property(self):
+        """
+        Test that defining a schema containing a keyword with invalid
+        properties raises a SchemaError.
+        """
+
+        def make_invalid_schema():
+            class InvalidSchema(fits.Schema):
+                keywords = {
+                    'TEST': {'kqwijibo': True}
+                }
+
+        assert_raises(SchemaDefinitionError, make_invalid_schema)
+
+    def test_mandatory_keywords(self):
+        """
+        Basic test of mandatory keyword validation.
+        """
+
+        class TestSchema(fits.Schema):
+            keywords = {
+                'TEST1': {'mandatory': True},
+                'TEST2': {'mandatory': False}
+            }
+
+        h1 = fits.Header([('TEST1', '')])  # no TEST2
+        h2 = fits.Header([('TEST1', ''), ('TEST2', '')])
+        h3 = fits.Header([('TEST2', '')])
+
+        assert TestSchema.validate(h1)
+        assert TestSchema.validate(h2)
+        assert_raises(SchemaValidationError, TestSchema.validate, h3)
